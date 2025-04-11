57e2d81c96f6aa62675d3058ed2225339c47f8e8
Chamoda Karunathilaka <it21268076@my.sliit.lk>
2025-04-11 18:04:22 +0530

    Added Dto for request and response

---
+++ src/main/java/com/example/authservice/dto/request/SignupRequest.java
@@ -0,0 +1,34 @@
+package com.example.authservice.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 100)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    @NotBlank
+    @Size(max = 100)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 100)
+    private String lastName;
+
+    private Set<String> roles;
+}
